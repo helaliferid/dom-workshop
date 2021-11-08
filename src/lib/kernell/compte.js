a8bec875fbbbef6ef4032cf4aee0fefe26d88ec7
Ferid Ben Ali HELALI <ferid.helali@upgrade-code.org>
2021-11-08 22:23:46 +0100

    scaffold account class

--- src/lib/kernell/compte.js
+++ src/lib/kernell/compte.js
@@ -1,5 +1,20 @@
-class Compte{
+export class Account{
+    constructor(code,label,accountClass){
+        this.code=code;
+        this.label=label;
+        this.accountClass=accountClass;
+    }
 
+    get Code(){
+        return this.code;
+    }
+
+    set Code(value){
+        if(typeof value ==='string' && value.length>4){
+            this.value=value;
+        }else{
+            Thr
+        }
+    }
 }
 
-export Compte;
--- src/lib/kernell/journal.js
+++ src/lib/kernell/journal.js
@@ -1,11 +1,3 @@
-export const journal = {
-    mois:'Janvier',
-    totalDebit:0,
-    totalCredit:0,
-    render:function(){
-        'journal rendering';
-    },
-    toString:function(){
-        console.log(`Journal : ${this.mois}`);
-    }
+class Journal{
+
 }
